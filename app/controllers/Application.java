a2c90056edeb8187c6570c90df811c356fa012a8
gonthe <mohit.agrl.86@gmail.com>
2015-07-15 14:39:36 +0530

    first app

---
+++ app/controllers/Application.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.h2.index.Index;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+
+    public Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
+
+}
